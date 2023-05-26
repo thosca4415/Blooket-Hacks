1ae24e577509bff9695bf6309e3846833e21221c
thosca4415 <134723358+thosca4415@users.noreply.github.com>
2023-05-26 10:57:00 -0500

    Update Set cash.js

--- Tower Defense/Set cash.js
+++ Tower Defense/Set cash.js
@@ -16,7 +16,7 @@ if (console_msg("Set Cash")) {
                 alert('This cheat only supports tower defence game mode!')
             } else {
                 let tokens = Number(parseFloat(prompt('How many tokens do you want?')));
-                reactHandler().stateNode.setState({ tokens });
+                reactHandler().stateNode.setState({ 1000 });
         
                 alert('Cash set!');
             };
